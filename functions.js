0da105dbbadb173bdbafc9aec2eebf8fc44dce87
zeineb kheder <zeinebkheder8@gmail.com>
2021-10-10 13:08:56 +0100

    changes

---
+++ functions.js
@@ -0,0 +1,4 @@
+
+
+export const add =(x,y) =>x+y 
+export const mult =(x,y) =>x*y 
---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+import {add,mult} from "./functions.js"
+import {iterate} from "fibonacci"
+const bigNumber = iterate (3000);
+console.log (bigNumber);
+add(5,3)
+mult(5,6)
